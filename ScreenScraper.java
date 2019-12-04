5db0b7e61f6c5d6369d883f28d618826405175da
MaierhoferM <45440955+MaierhoferM@users.noreply.github.com>
2019-12-03 22:52:18 -0600

    This is currently not being used in the main

--- ScreenScraper.java
+++ ScreenScraper.java
@@ -1,16 +1,26 @@
 //De Lay and Maierhofer 
 //screenscraper this actually takes in the website
+//OOPFinal
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
 public class ScreenScraper {
-	public static void main(String[] args) {
-		
-        System.out.print("Enter url for website: ");
-        Scanner sc = new Scanner(System.in);
-        String addr = sc.nextLine();
-        String line;
+	public static void Scrape(WebScraper frm) {
+		JLabel label = new JLabel("Enter URL:");
+		JTextField urlToSearch = new JTextField(30);
+		JButton btnFetch = new JButton ("Fetch");
         
-        try {
+		/*System.out.print("Enter url for website: ");
+        *Scanner sc = new Scanner(System.in);
+        *String addr = sc.nextLine();
+        *String line;
+        *try {
         	
             URL link = new URL(addr);
             Scanner linksc = new Scanner(link.openStream());
@@ -20,16 +30,21 @@ public class ScreenScraper {
             }
             /*
              * Takes in the chosen website from the user
-             * and if unable to conect it will notify the user 
-             */
+             * and if unable to connect it will notify the user 
+             *
             
             linksc.close(); //closes 
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Could not connect to chosen website."
-            		+ "Use different website or try again");
-        }
-         
+        /*   		+ "Use different website or try again");
+      */
+        btnFetch.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		
+        	}
+        });
     }
 }
+
 
