286a74d82606edf95461bf92693fd4aa8119d480
Emily De Lay <46684137+emilydelay86@users.noreply.github.com>
2019-12-05 10:02:22 -0600

    adding getters and setters

--- data.java
+++ data.java
@@ -1,4 +1,28 @@
 //the getters and setters will go in this class
 public class data {
-	private String TxtBox, Text;
+	private String TxtBox, Text, data;
+
+	public String getTxtBox() {
+		return TxtBox;
+	}
+
+	public void setTxtBox(String txtBox) {
+		TxtBox = txtBox;
+	}
+
+	public String getText() {
+		return Text;
+	}
+
+	public void setText(String text) {
+		Text = text;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
 }
