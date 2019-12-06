bcc3a04023e68703b428f9bbf4dd4723ba3dbcb8
Emily De Lay <46684137+emilydelay86@users.noreply.github.com>
2019-12-06 13:29:19 -0600

    getters and setters, to string, and the constructor

--- data.java
+++ data.java
@@ -1,32 +1,66 @@
 //the getters and setters will go in this class
 public class data {
-	private String TxtBox, Text, data;
+	private String name, number, animal, gender, breed, age, id;
 
-	public String getTxtBox() {
-		return TxtBox;
+	public String getName() {
+		return name;
 	}
-
-	public void setTxtBox(String txtBox) {
-		TxtBox = txtBox;
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public String getText() {
-		return Text;
+	public String getNumber() {
+		return number;
 	}
-
-	public void setText(String text) {
-		Text = text;
+	public void setNumber(String number) {
+		this.number = number;
 	}
-
-	public String getData() {
-		return data;
+	public String getAnimal() {
+		return animal;
 	}
-
-	public void setData(String data) {
-		this.data = data;
+	public void setAnimal(String animal) {
+		this.animal = animal;
 	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	//gotta make the constructor 
+	public data(String name, String number, String animal,
+			String gender, String breed, String age, String id) {
+	//setfunction(variable)
+		setName(name);
+		setNumber(number);
+		setAnimal(animal);
+		setGender(gender);
+		setBreed(breed);
+		setAge(age);
+		setId(id);
+		
+	}
+	
 	@Override
 	public String toString() {
-		return String.format("%s, %s", TxtBox, data);
+		return String.format("%s, %s, %s, %s, %s, %s",
+				name, number, animal, gender, breed, age, id );
 	}
 }
