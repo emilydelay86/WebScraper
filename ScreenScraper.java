fff6efa5868cc327c2927803b666995ce4fadc7b
MaierhoferM <45440955+MaierhoferM@users.noreply.github.com>
2019-12-09 14:33:37 -0600

    Update ScreenScraper.java

--- ScreenScraper.java
+++ ScreenScraper.java
@@ -1,45 +1,54 @@
-  
 //De Lay and Maierhofer 
 //screenscraper this actually takes in the website
 //OOPFinal
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 public class ScreenScraper {
-	public static ArrayList<ScreenScraper> Scrape(WebScraper frm) {
+	public static ArrayList<ScreenScraper> Scrape (WebScraper frm) {
 		//JLabel label = new JLabel("Enter URL:");
 		//JTextField urlToSearch = new JTextField(30);
 		//JButton btnFetch = new JButton ("Fetch");
         
 		//System.out.print("Enter url for website: ");
 		//btnFetch = JButton;
-        Scanner sc = new Scanner(System.in);
-        String addr = sc.nextLine();
-        String line;
+        //JTextField urlToSearch = null;
         try {
     		//trying to create the array list for the data
     		ArrayList<ScreenScraper> datapulled = new ArrayList<ScreenScraper>();
-    		
-            URL link = new URL(addr);
-            Scanner linksc = new Scanner(link.openStream());
-            while (linksc.hasNextLine()) {
-                line = linksc.nextLine();
-                System.out.println(line);
+			//URL link = new URL(urlToSearch.getText());
+    		URL url = new URL(urlToSearch.getText());
+            URLConnection uconn = url.openConnection();
+            BufferedReader br = new BufferedReader(
+            		new InputStreamReader(uconn.getInputStream()));
+            
+            String urlText = "";
+            String line;
+            while((line = br.readLine()) != null){ 
+            //while there is a line in the bufferedreader and it's not empty
+            urlText += line;//add that line to the text of the URL 
+            System.out.print(urlText);
             }
+           
             
              // Takes in the chosen website from the user
              // and if unable to connect it will notify the user 
             
             
-            linksc.close(); //closes 
+            br.close(); //closes 
             return datapulled; 
+            
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Could not connect to chosen website."
@@ -63,6 +72,11 @@ public class ScreenScraper {
 }
 */
 }
+
+
+
+
+
 
 
 
